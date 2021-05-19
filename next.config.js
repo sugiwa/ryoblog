cb5b546d973dd725bcab40ec50ee575b741b86b8
wakasugir <wakasugi.ryota.infratop@gmail.com>
2021-05-19 20:38:48 +0900

    add PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
 module.exports = {
     images: {
       domains: ['images.microcms-assets.io'],
     },
-  }
+}
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+})
